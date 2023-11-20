3dd416379ad6106c53ec75fdb2c271a57075742e
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-11-20 21:58:14 +0530

    init foundry fundme

---
+++ test/FundMe.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identfier:MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console2} from "../lib/forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
